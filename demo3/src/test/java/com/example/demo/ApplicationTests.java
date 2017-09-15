9010ff0c277eb8d2e9c96b38929b4486e96359ae
hemingliang <916812579@qq.com>
2017-09-15 11:00:00 +0800

    Spring Boot中使用Swagger2构建强大的RESTful API文档

--- demo3/src/test/java/com/example/demo/ApplicationTests.java
+++ demo3/src/test/java/com/example/demo/ApplicationTests.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
- 
-
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,68 +15,53 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.example.demo.web.UserController;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*; 
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootApplication
 public class ApplicationTests {
-	
-	private MockMvc mvc; 
-	 
-	@Before 
-	public void setUp() throws Exception { 
-		mvc = MockMvcBuilders.standaloneSetup(new UserController()).build(); 
-	} 
- 
-	@Test 
-	public void testUserController() throws Exception { 
-        // 测试UserController 
-		RequestBuilder request = null; 
- 
-		// 1、get查一下user列表，应该为空 
-		request = get("/users/"); 
-		mvc.perform(request) 
-				.andExpect(status().isOk()) 
-				.andExpect(content().string(equalTo("[]"))); 
- 
-		// 2、post提交一个user 
-		request = post("/users/") 
-				.param("id", "1") 
-				.param("name", "测试大师") 
-				.param("age", "20"); 
-		mvc.perform(request) 
-		        .andExpect(content().string(equalTo("success"))); 
- 
-		// 3、get获取user列表，应该有刚才插入的数据 
-		request = get("/users/"); 
-		mvc.perform(request) 
-				.andExpect(status().isOk()) 
-				.andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"age\":20}]"))); 
- 
-		// 4、put修改id为1的user 
-		request = put("/users/1") 
-				.param("name", "测试终极大师") 
-				.param("age", "30"); 
-		mvc.perform(request) 
-				.andExpect(content().string(equalTo("success"))); 
- 
-		// 5、get一个id为1的user 
-		request = get("/users/1"); 
-		mvc.perform(request) 
-				.andExpect(content().string(equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"age\":30}"))); 
- 
-		// 6、del删除id为1的user 
-		request = delete("/users/1"); 
-		mvc.perform(request) 
-				.andExpect(content().string(equalTo("success"))); 
- 
-		// 7、get查一下user列表，应该为空 
-		request = get("/users/"); 
-		mvc.perform(request) 
-				.andExpect(status().isOk()) 
-				.andExpect(content().string(equalTo("[]"))); 
- 
-	} 
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
+	}
+
+	@Test
+	public void testUserController() throws Exception {
+		// 测试UserController
+		RequestBuilder request = null;
+
+		// 1、get查一下user列表，应该为空
+		request = get("/users/");
+		mvc.perform(request).andExpect(status().isOk()).andExpect(content().string(equalTo("[]")));
+
+		// 2、post提交一个user
+		request = post("/users/").param("id", "1").param("name", "测试大师").param("age", "20");
+		mvc.perform(request).andExpect(content().string(equalTo("success")));
+
+		// 3、get获取user列表，应该有刚才插入的数据
+		request = get("/users/");
+		mvc.perform(request).andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"age\":20}]")));
+
+		// 4、put修改id为1的user
+		request = put("/users/1").param("name", "测试终极大师").param("age", "30");
+		mvc.perform(request).andExpect(content().string(equalTo("success")));
+
+		// 5、get一个id为1的user
+		request = get("/users/1");
+		mvc.perform(request).andExpect(content().string(equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"age\":30}")));
+
+		// 6、del删除id为1的user
+		request = delete("/users/1");
+		mvc.perform(request).andExpect(content().string(equalTo("success")));
+
+		// 7、get查一下user列表，应该为空
+		request = get("/users/");
+		mvc.perform(request).andExpect(status().isOk()).andExpect(content().string(equalTo("[]")));
+
+	}
 
 }
